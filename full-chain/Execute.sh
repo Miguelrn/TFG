9978669f57c6785b34bbc537f8d98fa1144a0253
Pomer <jmrnavarro@ucm.es>
2016-04-08 12:10:23 +0200

    Execution need new param

--- full-chain/Execute.sh
+++ full-chain/Execute.sh
@@ -1,3 +1,3 @@
-./full-chain ../Cuprite/Cuprite 29 0 64 1 1
-#./full-chain ../SubsetWTC/SubsetWTC 50 0 256 1 1
-#./full-chain ../Sintetica/Sintetica 50 0 512 1 1
+./full-chain ../Cuprite/Cuprite 29 0 64 1 2 e
+#./full-chain ../SubsetWTC/SubsetWTC 50 0 256 1 1 e
+#./full-chain ../Sintetica/Sintetica 50 0 512 1 1 e
